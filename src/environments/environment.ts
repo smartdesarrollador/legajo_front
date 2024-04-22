a631f267213ffa0f2b9221b61e140aa66d20962e
Jeans <sistemadesignstyle@gmail.com>
2024-04-21 22:25:38 -0500

    cambiando url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,20 @@
-/* export const environment = {
-  production: true,
-  apiUrl: 'https://apilegajo.amorinomoda.com/api/test_api_crud',
-  apiUrl2: 'https://apilegajo.amorinomoda.com/api/test_api',
-  apiUrl3: 'https://apilegajo.amorinomoda.com/api/file',
-  apiUrlTrabajador: 'https://apilegajo.amorinomoda.com/api/trabajador',
-  apiUrlRegister: 'https://apilegajo.amorinomoda.com/api/register',
-  apiUrlLogin: 'https://apilegajo.amorinomoda.com/api/login',
-  apiUrlEmail: 'https://apilegajo.amorinomoda.com/api/enviar_correo',
-  apiUrlEmpleador: 'https://apilegajo.amorinomoda.com/api/empleador',
-  apiUrlUltimoEmpleador:
-    'https://apilegajo.amorinomoda.com/api/ultimo_empleador',
-  apiUrlActividadUltimoEmpleador:
-    'https://apilegajo.amorinomoda.com/api/actividad_ultimo_empleador',
-}; */
-
 export const environment = {
+  production: true,
+  apiUrl: 'https://apilegajo.hawatech.agency/api/test_api_crud',
+  apiUrl2: 'https://apilegajo.hawatech.agency/api/test_api',
+  apiUrl3: 'https://apilegajo.hawatech.agency/api/file',
+  apiUrlTrabajador: 'https://apilegajo.hawatech.agency/api/trabajador',
+  apiUrlRegister: 'https://apilegajo.hawatech.agency/api/register',
+  apiUrlLogin: 'https://apilegajo.hawatech.agency/api/login',
+  apiUrlEmail: 'https://apilegajo.hawatech.agency/api/enviar_correo',
+  apiUrlEmpleador: 'https://apilegajo.hawatech.agency/api/empleador',
+  apiUrlUltimoEmpleador:
+    'https://apilegajo.hawatech.agency/api/ultimo_empleador',
+  apiUrlActividadUltimoEmpleador:
+    'https://apilegajo.hawatech.agency/api/actividad_ultimo_empleador',
+};
+
+/* export const environment = {
   production: true,
   apiUrl: 'https://back.iatecdigital.com/api/test_api_crud',
   apiUrl2: 'https://back.iatecdigital.com/api/test_api',
@@ -27,4 +27,4 @@ export const environment = {
   apiUrlUltimoEmpleador: 'https://back.iatecdigital.com/api/ultimo_empleador',
   apiUrlActividadUltimoEmpleador:
     'https://back.iatecdigital.com/api/actividad_ultimo_empleador',
-};
+}; */
